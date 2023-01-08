6b1f5d7419b06ea9968766f9208918a30fa2448a
Juan Sebastian Urrea Lopez <jurrea49@hotmail.com>
2023-01-08 13:39:36 -0500

    Model

---
+++ model/CourseSection.mjs
@@ -0,0 +1,32 @@
+/*
+Clase que representa una sección de una materia/curso
+*/
+
+import { Schedule } from "./Schedule.mjs";
+
+class CourseSection {
+
+    constructor({
+        title,       // Nombre del curso
+        courseCode,  // class + course, por ejemplo "ADMI2501"
+        credits,     // Número de créditos
+        seatsavail,  // Cupos disponibles
+        schedules,   // Horarios
+        instructors, // Profesores
+        nrc,         // Número único de registro 
+        campus,      // Campus
+        term,        // Período académico
+    }) {
+        this.title = title;
+        this.courseCode = courseCode;
+        this.credits = credits;
+        this.seatsavail = seatsavail;
+        this.schedule = new Schedule(schedules);
+        this.instructors = instructors;
+        this.nrc = nrc;
+        this.campus = campus;
+        this.term = term;
+    }
+}
+
+export { CourseSection };
