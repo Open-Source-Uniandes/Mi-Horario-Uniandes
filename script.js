920081fbb6a06f5c51b53eed1e8b22724ab27afc
Juan Sebastian Urrea Lopez <jurrea49@hotmail.com>
2023-01-10 02:35:26 -0500

    Step 1 view

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ const viewModel = new ViewModel();
 // Empieza a ejecutar incluso antes de cargar imágenes y CSS
 // Bind se asegura que se llame la función en el contexto del view
 window.addEventListener('DOMContentLoaded', viewModel.start.bind(viewModel));  
-
-//////////PRUEBA
-setTimeout(() => console.log(
-    viewModel.getCourseSections({
-        courseCode : "ISIS1105",
-        sections : ["1", "2", "3", "4"],
-    })
-), 30000)
