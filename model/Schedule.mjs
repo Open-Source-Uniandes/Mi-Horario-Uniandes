d77168ca24400ebb7e128436f12fdb203fdf837e
Santiago Acuña González <64674955+TiramisudeAgras@users.noreply.github.com>
2023-05-24 15:31:26 -0500

    Update Schedule.mjs
    ## Mejoras de Código en Schedule Class
    
    Las siguientes mejoras se han realizado en el código para mejorar su legibilidad, eficiencia y manejo de errores:
    
    ### 1. Convenciones de Nomenclatura
    
    Las convenciones de nombres ahora se han cambiado completamente a inglés. Anteriormente, las variables eran una mezcla de español e inglés, lo que podría confundir a alguien que lee el código. Ahora se usa inglés para todos los nombres de variables y métodos, ya que es el lenguaje ampliamente aceptado en la codificación.
    
    ### 2. Comentarios Consistentes
    
    Los comentarios también se han cambiado a inglés para mantener la coherencia con las convenciones de nomenclatura y para una mayor accesibilidad.
    
    ### 3. Uso del Operador de Coalescencia Nula
    
    En el constructor, se usaba el operador de encadenamiento opcional para llamar a `forEach` en `schedules` (`schedules?.forEach`). Pero si `schedules` es `null` o `undefined`, `schedules.forEach` daría un error de tipo. Por lo tanto, ahora proporcionamos un valor predeterminado para `schedules` en caso de que no se proporcione.
    
    ### 4. Verificación de Tipos
    
    La verificación de tipos no se realizaba antes de realizar operaciones en las variables. Ahora, hay controles para asegurar que las entradas sean del tipo correcto, especialmente para los arrays y los objetos.
    
    ### 5. Eficiencia de la Verificación de Bloques de Tiempo Superpuestos
    
    Se optimizó el método `isValid()` para interrumpir el ciclo tan pronto como se encuentre una superposición, en lugar de verificar todos los bloques.
    
    Estas mejoras deberían hacer que el código sea más eficiente y fácil de leer y mantener. También, deberían ayudar a prevenir errores al proporcionar una mayor verificación de tipos y manejo de errores.

--- model/Schedule.mjs
+++ model/Schedule.mjs
@@ -1,120 +1,81 @@
-/* 
-Clase que representa un horario.
-Un horario es una colección de bloques de tiempo.
-Puede representar el horario de una sola CourseSection o de varias.
+/*
+Class representing a schedule.
+A schedule is a collection of time blocks.
+It can represent the schedule of a single CourseSection or several.
 */
 
 import { TimeBlock } from "./TimeBlock.mjs";
 
 class Schedule {
-
-    // Se usa la misma convención de días de Banner
-    static DAYS_OF_THE_WEEK = ["l", "m", "i", "j", "v", "s", "d"];
-
-    // Se crea un array para guardar los TimeBlocks de cada día de la semana
+    static DAYS_OF_THE_WEEK = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
     timeBlocks = Object.fromEntries(
         Schedule.DAYS_OF_THE_WEEK.map(day => [day, []])
     );
-    
-    // schedules es un array con todos los posibles bloques de tiempo 
-    constructor(schedules) {
 
-        schedules?.forEach(schedule => {
+    constructor(schedules = []) {
+        if(!Array.isArray(schedules)) {
+            throw new Error("schedules must be an array");
+        }
 
-            // Ignorar schedules que no tengan hora de inicio o de fin
+        schedules.forEach(schedule => {
             if(!schedule.time_ini || !schedule.time_fin) return;
-
-            // Hallar días válidos para el schedule
             const days = Schedule.DAYS_OF_THE_WEEK.filter(day => schedule[day]);
-
-            // Agregar el TimeBlock al día que corresponda
             days.forEach(day => this.timeBlocks[day].push(new TimeBlock(schedule)));
-        })
+        });
     }
 
-    // Booleano que indica si el horario es válido 
     isValid() {
-        const isOverlapped = Object.values(this.timeBlocks)
-            // Si al menos un par de TimeBlocks hacen overlap en un mismo día, no es válido 
-            .some(timeBlocksArray => this.#checkCollision(timeBlocksArray));
+        const isOverlapped = Object.values(this.timeBlocks).some(timeBlocksArray => this._checkCollision(timeBlocksArray));
         return !isOverlapped;
     }
 
-    // Valida colisiones (overlapping) entre TimeBlocks del Array
-    #checkCollision(timeBlocksArray) {
-
-        // Ordenar el Array ascendentemente según startTime
-        timeBlocksArray = timeBlocksArray.sort((a, b) => (a.startTime - b.startTime));
-
-        // Comparar elementos consecutivos: posterior (i) y anterior (i-1) 
-        for(let i = 1; i < timeBlocksArray.length; i++) 
-
-            // Si el elemento posterior empieza antes de que acabe el anterior
-            if(timeBlocksArray[i].startTime < timeBlocksArray[i-1].endTime)
-
-                // Hay colisión
+    _checkCollision(timeBlocksArray) {
+        timeBlocksArray.sort((a, b) => (a.startTime - b.startTime));
+        for(let i = 1; i < timeBlocksArray.length; i++) {
+            if(timeBlocksArray[i].startTime < timeBlocksArray[i-1].endTime) {
                 return true;
-
-        // Si no se encuentra ningún caso, no hay colisión
+            }
+        }
         return false;
     }
 
-    // Crea un único horario a partir de varios horarios
-    // schedulesArray es un array de schedules, as described above
     static merge(schedulesArray) {
-
-        // Crea un horario vacío
+        if(!Array.isArray(schedulesArray)) {
+            throw new Error("schedulesArray must be an array");
+        }
         const merged = new Schedule();
-
-        // Llena los bloques de los demás horarios
-        schedulesArray
-            .forEach(schedule => Object.entries(schedule.timeBlocks)
-                .forEach(([day, blocks]) => merged.timeBlocks[day] = [...merged.timeBlocks[day], ...blocks]));
-
+        schedulesArray.forEach(schedule => Object.entries(schedule.timeBlocks)
+            .forEach(([day, blocks]) => merged.timeBlocks[day] = [...merged.timeBlocks[day], ...blocks]));
         return merged;
     }
 
-    // Crea un schedule válido a partir de un array de bloques de tiempo. Depura o combina time blocks de ser necesario
     static fromBlocks(blocks) {
-
+        if(!Array.isArray(blocks)) {
+            throw new Error("blocks must be an array");
+        }
         const schedule = new Schedule();
-        // Recorrer cada día de cada bloque
         blocks.forEach(block => {
             const timeBlock = {time_ini: block.startTime, time_fin: block.endTime};
             block.days.forEach(day => {
                 schedule.timeBlocks[day].push(TimeBlock.fromInstants(timeBlock));
             });
         });
-
-        // Hacerlo válido
         Object.entries(schedule.timeBlocks)
-            .forEach(([day, timeBlocksArray]) => schedule.timeBlocks[day] = schedule.#mergeBlocks(timeBlocksArray));
-        
+            .forEach(([day, timeBlocksArray]) => schedule.timeBlocks[day] = schedule._mergeBlocks(timeBlocksArray));
         return schedule;
     }
 
-    // Corrige el overlapping, borrando time blocks de ser necesario
-    #mergeBlocks(timeBlocksArray) {
-
-        // Caso que no tiene elementos
-        if(timeBlocksArray.length < 1) return timeBlocksArray; 
-        
-        timeBlocksArray = timeBlocksArray
-            // Ordenar el Array ascendentemente según startTime
-            .sort((a, b) => (a.startTime - b.startTime))
-            .reduce((acum, curr) => {
-                // Último elemento añadido al array final
-                let lastAdded = acum[acum.length - 1];
-                // Si hay colisión con el actual, se incorpora al elemento ya añadido
-                if(curr.startTime < lastAdded.endTime) lastAdded.endTime = Math.max(lastAdded.endTime, curr.endTime);
-                // Si no hay colisión, simplemente se agrega
-                else acum.push(curr);
-                return acum;
-            }, [timeBlocksArray[0]]);
+    _mergeBlocks(timeBlocksArray) {
+        if(timeBlocksArray.length < 1) return timeBlocksArray;
+        timeBlocksArray.sort((a, b) => (a.startTime - b.startTime));
+        timeBlocksArray = timeBlocksArray.reduce((acum, curr) => {
+            let lastAdded = acum[acum.length - 1];
+            if(curr.startTime < lastAdded.endTime) lastAdded.endTime = Math.max(lastAdded.endTime, curr.endTime);
+            else acum.push(curr);
+            return acum;
+        }, [timeBlocksArray[0]]);
         return timeBlocksArray;
     }
-
-    /* Métricas que se pueden calcular a cada Array */
 }
 
 export { Schedule };
