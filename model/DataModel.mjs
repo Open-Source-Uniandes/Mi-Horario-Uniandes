5ddb8a3517b1d9ad520b55d99f22e463a107fb2c
Daniel Diaz <110594320+ddi4z@users.noreply.github.com>
2023-07-12 21:11:01 -0500

    Search courses by name

--- model/DataModel.mjs
+++ model/DataModel.mjs
@@ -3,6 +3,7 @@ Este módulo se encarga de leer y procesar la información de la API
 */
 
 import { CourseSection } from "./CourseSection.mjs";
+import { Trie } from "./Trie.mjs";
 
 class DataModel {
 
@@ -14,11 +15,21 @@ class DataModel {
 
         // Obtener datos (aprox 20 segundos)
         this.lastTime = new Date().toLocaleString();
+        this.trie = new Trie();
         this.data = await fetch(DataModel.API)
             .then(response => response.json())
             // Añadir el atributo courseCode = class + course
-            .then(response => response.map(response => ({...response, courseCode : response.class + response.course})))
-            .then(response => response.map(response => new CourseSection(response)));
+            .then(response => response.map(response => ({ ...response, courseCode: response.class + response.course })))
+            .then(response => response.map(response => {
+            // Eliminar espacios adicionales en el título
+            response.title = response.title.replace(/\s+/g, ' ').trim();
+            // Eliminar todas las tildes
+            response.title = response.title.replace('Á', 'A').replace('É', 'E').replace('Í', 'I').replace('Ó', 'O').replace('Ú', 'U');
+            return new CourseSection(response);
+            }));
+            for (let course of this.data) {
+                this.trie.insert(course.title);
+            }
     }
 }
 
