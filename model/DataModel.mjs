7da463f6a3bc2d5765a9e1f3866862a6db693d8a
SA <64674955+TiramisudeAgras@users.noreply.github.com>
2023-07-22 13:15:41 +0200

    Update DataModel.mjs
    "Refactorización de la clase DataModel para mejorar el rendimiento reduciendo recorridos del array y consolidando operaciones de transformación de strings."

--- model/DataModel.mjs
+++ model/DataModel.mjs
@@ -1,33 +1,47 @@
-/*
-Este módulo se encarga de leer y procesar la información de la API
-*/
-
+// Importa la clase CourseSection
 import { CourseSection } from "./CourseSection.mjs";
 
+// Define la clase DataModel
 class DataModel {
 
-    // Endpoint de cursos registrados en Banner
+    // Define la URL de la API como una constante estática
     static API = 'https://ofertadecursos.uniandes.edu.co/api/courses';
 
-    // Carga los datos desde la API
+    // Método para cargar los datos
     async loadData() {
-
-        // Obtener datos (aprox 20 segundos)
+        // Almacena la fecha y hora actual
         this.lastTime = new Date().toLocaleString();
+        
+        // Hace una petición a la API, procesa la respuesta y almacena los datos
         this.data = await fetch(DataModel.API)
             .then(response => response.json())
-            // Añadir el atributo courseCode = class + course
-            .then(response => response.map(response => ({ ...response, courseCode: response.class + response.course })))
-            .then(response => response.map(response => {
-                // Eliminar espacios adicionales en el título
-                response.title = response.title.replace(/\s+/g, ' ').trim();
-                // Eliminar todas las tildes
-                response.title = response.title.replace('Á', 'A').replace('É', 'E').replace('Í', 'I').replace('Ó', 'O').replace('Ú', 'U');
-                // Convertir a mayúsculas
-                response.title = response.title.toUpperCase();
-                return new CourseSection(response);
-            }));
+            .then(response => response.map(this.processResponseData));
+    }
+
+    // Método para procesar cada objeto de datos de la respuesta
+    processResponseData(responseData) {
+        // Crea un nuevo atributo, courseCode, que es la concatenación de class y course
+        responseData.courseCode = responseData.class + responseData.course;
+        
+        // Limpia y normaliza el atributo title
+        responseData.title = responseData.title
+            // Reemplaza cualquier secuencia de espacios con un solo espacio
+            .replace(/\s+/g, ' ').trim()
+            // Reemplaza las vocales con tildes por vocales sin tildes
+            .replace(/[ÁÉÍÓÚ]/g, function(c) {
+                return c === 'Á' ? 'A' :
+                    c === 'É' ? 'E' :
+                    c === 'Í' ? 'I' :
+                    c === 'Ó' ? 'O' :
+                    c === 'Ú' ? 'U' : c;
+            })
+            // Convierte el título a mayúsculas
+            .toUpperCase();
+        
+        // Devuelve un nuevo objeto CourseSection basado en el objeto de datos procesado
+        return new CourseSection(responseData);
     }
 }
 
+// Exporta la clase DataModel
 export { DataModel };
