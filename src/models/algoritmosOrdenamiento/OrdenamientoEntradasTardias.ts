262b5b7dbebaabf31805c6396997c54cf2a2b31e
jmpizza <jespitiap@unal.edu.co>
2024-08-14 15:22:35 -0500

    Definicion de metodos de ordenamiento para entradas tardias y salidas tempranas

--- src/models/algoritmosOrdenamiento/OrdenamientoEntradasTardias.ts
+++ src/models/algoritmosOrdenamiento/OrdenamientoEntradasTardias.ts
@@ -2,7 +2,19 @@ import OrdenamientoHorarios from '@/models/algoritmosOrdenamiento/OrdenamientoHo
 import Horario from '@/models/Horario';
 
 export default class OrdenamientoEntradasTardias implements OrdenamientoHorarios{
-  ordernar(horarios: Horario[]) : void {
-    
+  ordernar(horarios: Horario[]): void {
+    horarios.sort((a, b) => {
+      return this.sumaHorasEntrada(b) - this.sumaHorasEntrada(a);
+    });
+  }
+  
+  private sumaHorasEntrada(horario: Horario): number {
+    return horario.secciones.reduce((total, seccion) => {
+      return total + this.minHoraInicio(seccion.horarios);
+    }, 0);
+  }
+  
+  private minHoraInicio(bloques: { horaInicio: number }[]): number {
+    return bloques.reduce((min, bloque) => Math.min(min, bloque.horaInicio), 24*60);
   }
 }
--- src/models/algoritmosOrdenamiento/OrdenamientoSalidasTempranas.ts
+++ src/models/algoritmosOrdenamiento/OrdenamientoSalidasTempranas.ts
@@ -2,7 +2,19 @@ import OrdenamientoHorarios from '@/models/algoritmosOrdenamiento/OrdenamientoHo
 import Horario from '@/models/Horario';
 
 export default class OrdenamientoSalidasTempranas implements OrdenamientoHorarios{
-  ordernar(horarios: Horario[]) : void {
-    
+  ordernar(horarios: Horario[]): void {
+    horarios.sort((a, b) => {
+      return this.sumaHorasSalida(a) - this.sumaHorasSalida(b);
+    });
+  }
+
+  private sumaHorasSalida(horario: Horario): number {
+    return horario.secciones.reduce((total, seccion) => {
+      return total + this.maxHoraFin(seccion.horarios);
+    }, 0);
+  }
+
+  private maxHoraFin(bloques: { horaFin: number }[]): number {
+    return bloques.reduce((max, bloque) => Math.max(max, bloque.horaFin), 0);
   }
 }
