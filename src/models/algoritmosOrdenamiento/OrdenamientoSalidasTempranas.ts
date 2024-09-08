1340b0eabca87c47c50e0d3824492f4be139464a
jmpizza <jespitiap@unal.edu.co>
2024-09-08 18:14:55 -0500

    Documentación de los metodos de ordenamiento para entradas tardias y salidas tempranas

--- src/models/algoritmosOrdenamiento/OrdenamientoSalidasTempranas.ts
+++ src/models/algoritmosOrdenamiento/OrdenamientoSalidasTempranas.ts
@@ -9,12 +9,24 @@ export default class OrdenamientoSalidasTempranas implements OrdenamientoHorario
     });
   }
 
+  /*
+    Calcula la suma de las horas de salida de un horario dado.
+
+    @param horario - El horario para el cual se calculará el total de horas de salida.
+    @return La suma de las horas de salida en minutos.
+  */
   private sumaHorasSalida(horario: Horario): number {
     return horario.secciones.reduce((total, seccion) => {
       return total + this.maxHoraFin(seccion.horarios);
     }, 0);
   }
 
+  /*
+    Calcula la hora de salida más tardia de una seccion dada.
+
+    @param bloques - Los bloques de tiempo de un dia especifico.
+    @return El tiempo de salida mas tarde en minutos.
+  */
   private maxHoraFin(bloques: BloqueTiempo[]): number {
     return bloques.reduce((max, bloque) => Math.max(max, bloque.horaFin), 0);
   }
